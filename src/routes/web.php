7a8b4180119a89be612e1d7af5c1ecc7f133499b
kiyo123456 <kiyoqingshui216@gmail.com>
2025-05-24 11:44:02 +0900

    week34

---
+++ src/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\WebsiteController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/website', function () {
+    return view('website'); // resources/views/website.blade.phpを探す
+})->name('website');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
